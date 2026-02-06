80dd97399e99f83666b17b391b273d4eda8be41d
Yuvan1532 <yuvanrajs.bsc@gmail.com>
2026-02-06 09:33:21 +0000

    Create index.php

---
+++ app/v1/index.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>StreamLine v1</title>
+</head>
+<body style="font-family: Arial; background-color: #cfe8ff;">
+    <h1>Welcome to Streamline - v1</h1>
+    <p><b>Server IP:</b>
+        <?php echo $_SERVER['SERVER_ADDR']; ?>
+    </p>
+</body>
+</html>
